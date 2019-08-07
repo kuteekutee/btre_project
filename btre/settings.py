53deb1a622dcfab02ff3ef5c1bf56d12de94dbaf
Bernard Sng <bernardsng@gmail.com>
2019-08-07 15:33:48 +0800

    Added local settings import

--- btre/settings.py
+++ btre/settings.py
@@ -148,3 +148,8 @@ EMAIL_PORT = 587
 EMAIL_HOST_USER ='bernardsng@gmail.com'
 EMAIL_HOST_PASSWORD ='bskk@3621'
 EMAIL_USE_TLS =True
+
+try:
+    from .local_settings import *
+except ImportError:
+    pass
